ffd3bc0e730d1634e70e749fcb15aefef5aef107
Srikrushna Pal <Srikrushna Pal>
2016-01-24 18:02:23 +0530

    added project files!!! excited..

---
+++ contactus.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Regional Transport Office</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link href="css/image-slider.css" rel="stylesheet" type="text/css" />
+    <script src="js/js-image-slider.js" type="text/javascript"></script>
+    
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="3/ninja-slider.css" rel="stylesheet" />
+    <script src="3/ninja-slider.js"></script>
+    <link href="3/thumbnail-slider.css" rel="stylesheet" type="text/css" />
+    <script src="3/thumbnail-slider.js" type="text/javascript"></script>
+</head>
+
+<body id="home">
+<?php include "header.php"; ?>
+
+
+<div id="wrapper">
+<div id="baner">
+<img src="images/contactus copy.jpg" width="100%" height="350px">
+<img src="images/contactus1.jpg" width="100%" height="150px">
+
+</div>
+<div id="contact" style="background-color:#999">
+<pre style="font-size:1.3em; text-align:center">
+<h1 style="color:white"><u>CONTACT US</u> </h1>
+Address :     REGIONAL TRANSPORT OFFICE
+  		       NAKTIGUDA, BHAWANIPATNA, KALAHANDI
+PIN- 766001
+
+Phone No :    06670-230545		
+   
+Email Id :     rto_kalahandi@yahoo.co.in
+   
+Website  :				
+   
+   </pre>
+</div>
+</div>
+<!-- fotter -->
+<footer>
+<div id="ff">
+<div id="one">
+<h2 id="ff">About US</h2>
+				
+				<li id="ff"><a id="ff" href="missions_and_vision.html">Missions & Vision</a></li>
+                                <li id="ff"><a id="ff" href="organogram.html">Organogram</a></li>
+                                <li id="ff"><a id="ff" href="who_is_who.html">Who is Who</a></li>
+				<li id="ff"><a id="ff" href="telephone_directory.html">Telephone Directory</a></li>
+				<li id="ff"><a id="ff" href="incumbency_list.html">Incumbency List</a></li>
+				<li id="ff"><a id="ff" href="#">Modernization Initiative </a></li>
+</div>
+<div id="two">
+<h2 id="ff">Services</h2>
+
+                <li id="ff"><a id="ff" target="_blank" href="http://oltp1.orissatransport.org/ODTS/Default.aspx">Postal Despatch Tracking</a></li>
+                                <li id="ff"><a id="ff" href="#">IT Despatch Tracking</a></li>
+				<li id="ff"><a id="ff" href="pmu.html">PMU</a></li>
+				<li id="ff"><a id="ff" href="#">Traffic Park </a></li>
+</div>
+<div id="three">
+<h2 id="ff">Establishment</h2>
+				<li id="ff"><a id="ff" href="driving_test_track.html">Driving Testing Track</a></li>
+                                <li id="ff"><a id="ff" href="#">RTO Building Status</a></li>
+				<li id="ff"><a id="ff" href="#">HMV Driving Testing Institute</a></li>
+				<li id="ff"><a id="ff" href="#">Inspection & Certification Centre</a></li>
+</div>
+<div id="four">
+
+<h2 id="ff">Equipments</h2>
+				<li id="ff"><a id="ff" href="driving_test_by_simulator.html">Driving Test By Simulator</a></li>
+                                <li id="ff"><a id="ff" href="#">Weigh Bridges (Static)</a></li>
+				<li id="ff"><a id="ff" href="#">Pollution Testing Machine</a></li>
+				<li id="ff"><a id="ff" href="#">Portable - WB</a></li>
+</div>
+<div id="five">
+<h2 id="ff">Enforcement</h2>
+				<li id="ff"><a id="ff" href="enforcement.html">Enforcement Team</a></li>
+</div>
+
+<div id="follow">
+<div id ="twi"><h2 id="ff">Follow US  :</h2></div>
+<div id="facebook">
+			<a id="ficon" href=""><img src="images/facebook.png"></a>
+			<a id="ficon" href=""><img src="images/twitter.png"</a>
+			<a id="ficon" href="#"><img src="images/rss.png"> </a></div>
+
+<div id="flink">
+			<li id="flink"><a id="flink" href="index.html">Home</a></li>
+			<li id="flink"><a id="flink" href="feedback.html">Feedback</a></li>
+			<li id="flink"><a id="flink" href="contact.html">Contact Us</a></li>
+			<li id="flink"><a id="flink" href="faq.html">FAQ</a></li>
+			<li id="flin"><a id="flink" href="#">RTI</a></li>
+			<p style="color:#A4A4A4;">Copyrights &copy; 2016.All rights reserved by RTO, Nuapada</p>
+</div>
+</div>
+
+<div style="float:right;margin-top:60px;">
+<div style="float:left"><h2 id="ff">Visitors  :</h2></div>
+
+<img src="http://hitwebcounter.com/counter/counter.php?page=6107128&style=0006&nbdigits=9&type=ip&initCount=0"  style=" border:10px solid #DEDEDE;" >
+
+  
+    
+  </div>
+</div>
+</footer>
+ <!--footer end -->
+
+
+</div>
+</body>
+
+</html>
